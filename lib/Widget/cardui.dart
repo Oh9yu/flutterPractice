c13734472113c48bcf00358ad577d8b40684e7bd
Oh9yu <kog6148@gmail.com>
2023-09-08 18:56:37 +0900

    ADD : flutter UI

--- lib/Widget/cardui.dart
+++ lib/Widget/cardui.dart
@@ -7,6 +7,7 @@ class CardUI extends StatelessWidget {
   final String loc;
   final Color titleColor;
   final Color locColor;
+  final IconData icon;
 
   const CardUI(
       {super.key,
@@ -15,19 +16,71 @@ class CardUI extends StatelessWidget {
       required this.cost,
       required this.loc,
       required this.titleColor,
-      required this.locColor});
+      required this.locColor,
+      required this.icon});
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      clipBehavior: Clip.hardEdge,
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
+        borderRadius: BorderRadius.circular(25),
         color: bgColor,
       ),
       child: Padding(
-        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-        child: Column(
-          children: [],
+        padding: EdgeInsets.symmetric(vertical: 25, horizontal: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  '$title',
+                  style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.w600,
+                      color: titleColor),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      '$cost',
+                      style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.w500,
+                          color: titleColor.withOpacity(0.8)),
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text(
+                      '$loc',
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: locColor,
+                          fontWeight: FontWeight.w600),
+                    ),
+                  ],
+                )
+              ],
+            ),
+            Transform.scale(
+              scale: 2.2,
+              child: Transform.translate(
+                offset: Offset(5, 15),
+                child: Icon(
+                  icon,
+                  color: titleColor,
+                  size: 88,
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -12,115 +12,140 @@ class App extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Color(0xFF181818),
-        body: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: 60,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Text(
-                        'HI OhGyu',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 28,
-                            fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        'Welcome!!',
-                        style: TextStyle(
-                          color: Colors.white.withOpacity(0.6),
-                          fontSize: 18,
-                        ),
-                      )
-                    ],
-                  )
-                ],
-              ),
-              SizedBox(
-                height: 80,
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 60,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
                   children: [
-                    Text(
-                      'Total Balance',
-                      style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white.withOpacity(0.5)),
-                    ),
-                    SizedBox(
-                      height: 5,
-                    ),
-                    Text(
-                      '971,028 KRW',
-                      style: TextStyle(
-                          fontSize: 36,
-                          fontWeight: FontWeight.w700,
-                          color: Colors.white.withOpacity(0.8)),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Button(
-                            text: 'Transfer',
-                            bgColor: Colors.amber,
-                            txColor: Colors.black),
-                        Button(
-                            text: 'Require',
-                            bgColor: const Color.fromARGB(96, 90, 90, 90),
-                            txColor: Colors.white)
-                      ],
-                    ),
-                    SizedBox(
-                      height: 50,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    Column(
                       crossAxisAlignment: CrossAxisAlignment.end,
                       children: [
                         Text(
-                          'Wallets',
+                          'HI OhGyu',
                           style: TextStyle(
                               color: Colors.white,
-                              fontSize: 30,
-                              fontWeight: FontWeight.w700),
+                              fontSize: 28,
+                              fontWeight: FontWeight.bold),
                         ),
                         Text(
-                          'View All',
+                          'Welcome!!',
                           style: TextStyle(
-                              fontSize: 16,
-                              color: Colors.white.withOpacity(0.5)),
-                        ),
+                            color: Colors.white.withOpacity(0.6),
+                            fontSize: 18,
+                          ),
+                        )
                       ],
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    CardUI(
-                        title: 'title',
-                        bgColor: Color(0xFF1F2123),
-                        cost: 'cost',
-                        loc: 'loc',
-                        titleColor: Colors.white,
-                        locColor: Colors.black.withOpacity(0.8))
+                    )
                   ],
                 ),
-              ),
-            ],
+                SizedBox(
+                  height: 80,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Total Balance',
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white.withOpacity(0.5)),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        '971,028 KRW',
+                        style: TextStyle(
+                            fontSize: 36,
+                            fontWeight: FontWeight.w700,
+                            color: Colors.white.withOpacity(0.8)),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Button(
+                              text: 'Transfer',
+                              bgColor: Colors.amber,
+                              txColor: Colors.black),
+                          Button(
+                              text: 'Require',
+                              bgColor: const Color.fromARGB(96, 90, 90, 90),
+                              txColor: Colors.white)
+                        ],
+                      ),
+                      SizedBox(
+                        height: 50,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text(
+                            'Wallets',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 30,
+                                fontWeight: FontWeight.w700),
+                          ),
+                          Text(
+                            'View All',
+                            style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.white.withOpacity(0.5)),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      CardUI(
+                          title: 'Euro',
+                          bgColor: Color(0xFF1F2123),
+                          cost: '6 428',
+                          loc: 'EUR',
+                          titleColor: Colors.white,
+                          locColor: Colors.grey,
+                          icon: Icons.euro_rounded),
+                      Transform.translate(
+                        offset: Offset(0, -20),
+                        child: CardUI(
+                            title: 'Dollar',
+                            bgColor: Colors.white,
+                            cost: '55 262',
+                            loc: 'USD',
+                            titleColor: Colors.black,
+                            locColor: Colors.black,
+                            icon: Icons.attach_money_rounded),
+                      ),
+                      Transform.translate(
+                        offset: Offset(0, -40),
+                        child: CardUI(
+                            title: 'Bit Coin',
+                            bgColor: Color(0xFF1F2123),
+                            cost: '320',
+                            loc: 'BTC',
+                            titleColor: Colors.white,
+                            locColor: Colors.grey,
+                            icon: Icons.currency_bitcoin_rounded),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
